c33990d8fb8d9af5dcf6d2cade81b898f0b17b25
Hackz1 <vg.xezo2007@gmail.com>
2022-08-08 21:23:53 -0400

    Created Player Bar Abstractions

--- src/StarterPlayerScripts/ConfigPlayerBar.lua
+++ src/StarterPlayerScripts/ConfigPlayerBar.lua
@@ -1,1 +1,19 @@
 -- Provides abstractions for interacting with player gui
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local PlayerBarGui = player:WaitForChild("PlayerGui"):WaitForChild("PlayerBarGui").PlayerBarsBackground
+
+local StaminaBar = PlayerBarGui.StaminaBarBackground.StaminaBar
+local HealthBar = PlayerBarGui.HealthBarBackground
+
+local ConfigPlayerBar = {}
+
+function ConfigPlayerBar.LessStamina(displayTime: number)
+    local ORIGINAL_BAR_COLOR = StaminaBar.BackgroundColor3
+    StaminaBar.BackgroundColor3 = Color3.fromRGB(172, 28, 28)
+    task.wait(displayTime)
+    StaminaBar.BackgroundColor3 = ORIGINAL_BAR_COLOR
+end
+
+return ConfigPlayerBar
